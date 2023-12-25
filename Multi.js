e2c168d1e62642e01fcb68ceb508b3aad8f3f913
Odorizzi <110546717+odorizzioficial@users.noreply.github.com>
2023-12-25 01:43:38 -0300

    Add files via upload

---
+++ Multi.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+const resultadoMultiplicacao = multiplicar(10, 5);
+const mensagemSaudacao = saudacao("Odorizzi");
+console.log(resultadoMultiplicacao);
+console.log(mensagemSaudacao);
---
+++ Multi.ts
@@ -0,0 +1,14 @@
+
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+const resultadoMultiplicacao = multiplicar(10, 5);
+const mensagemSaudacao = saudacao("Odorizzi");
+
+console.log(resultadoMultiplicacao); 
+console.log(mensagemSaudacao);      
